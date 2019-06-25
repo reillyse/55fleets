18b077794be23624d308ac8858b24cd2a4fb68e4
Sean Reilly <sean@letsgodo.it>
2019-06-25 14:25:05 -0700

    lets only flip active flwets

--- app/workers/fleet_flipper_scheduler.rb
+++ app/workers/fleet_flipper_scheduler.rb
@@ -2,7 +2,7 @@ class FleetFlipperScheduler
   include Sidekiq::Worker
 
   def perform
-    App.should_flip.each { |app|
+    App.active.should_flip.each { |app|
       FleetFlipper.perform_at(app.should_flip,app.id)
     }
   end
