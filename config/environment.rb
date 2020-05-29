6ddd6a68533df938c5bc13c173d4c7259384772a
Sean Reilly <reillyse@gmail.com>
2020-05-29 00:55:50 -0700

    midway

--- config/environment.rb
+++ config/environment.rb
@@ -1,16 +1,5 @@
 # Load the Rails application.
-require File.expand_path('../application', __FILE__)
+require_relative 'application'
 
 # Initialize the Rails application.
-ENV["AWS_REGION"] = "us-east-1"
-
-
-
-
-ENV["default_ami"] = "ami-04788916c74e6f3a2"
-
 Rails.application.initialize!
-
-Rails.application.configure do
-  config.log_tags = [:uuid, :remote_ip]
-end
--- config/environments/development.rb
+++ config/environments/development.rb
@@ -9,41 +9,54 @@ Rails.application.configure do
   # Do not eager load code on boot.
   config.eager_load = false
 
-  config.react.variant = :development
+  # Show full error reports.
+  config.consider_all_requests_local = true
 
-  # Show full error reports and disable caching.
-  config.consider_all_requests_local       = true
-  config.action_controller.perform_caching = false
+  # Enable/disable caching. By default caching is disabled.
+  # Run rails dev:cache to toggle caching.
+  if Rails.root.join('tmp', 'caching-dev.txt').exist?
+    config.action_controller.perform_caching = true
+    config.action_controller.enable_fragment_cache_logging = true
+
+    config.cache_store = :memory_store
+    config.public_file_server.headers = {
+      'Cache-Control' => "public, max-age=#{2.days.to_i}"
+    }
+  else
+    config.action_controller.perform_caching = false
+
+    config.cache_store = :null_store
+  end
+
+  # Store uploaded files on the local file system (see config/storage.yml for options).
+  config.active_storage.service = :local
 
   # Don't care if the mailer can't send.
   config.action_mailer.raise_delivery_errors = false
+
+  config.action_mailer.perform_caching = false
 
   # Print deprecation notices to the Rails logger.
   config.active_support.deprecation = :log
 
   # Raise an error on page load if there are pending migrations.
   config.active_record.migration_error = :page_load
-#  config.middleware.use Rack::SslEnforcer,  :except => proc { |env|  env['PATH_INFO'] == '/' && env['HTTP_USER_AGENT'].starts_with?('ELB-HealthChecker') }
+
+  # Highlight code that triggered database queries in logs.
+  config.active_record.verbose_query_logs = true
+
   # Debug mode disables concatenation and preprocessing of assets.
   # This option may cause significant delays in view rendering with a large
   # number of complex assets.
   config.assets.debug = true
-  config.log_level = :debug
-  # Asset digests allow you to set far-future HTTP expiration dates on all assets,
-  # yet still be able to expire them through the digest params.
-  config.assets.digest = true
 
-  # Adds additional error checking when serving assets at runtime.
-  # Checks for improperly declared sprockets dependencies.
-  # Raises helpful error messages.
-  config.assets.raise_runtime_errors = true
-#  config.active_job.queue_adapter = nil
-  # Raises error for missing translations
+  # Suppress logger output for asset requests.
+  config.assets.quiet = true
+
+  # Raises error for missing translations.
   # config.action_view.raise_on_missing_translations = true
 
-  ENV["REDIS_DB"] = "7"
-  Mongo::Logger.logger       = ::Logger.new('tmp/mongo.log')
-  Mongo::Logger.logger.level = ::Logger::INFO
-  ENV["BUILD_MACHINE_POOL_SIZE"] = "1"
-  ENV["BUILD_MACHINE_TYPES"] = "t3.medium,c4.large,m5.large"
+  # Use an evented file watcher to asynchronously detect changes in source code,
+  # routes, locales, etc. This feature depends on the listen gem.
+  config.file_watcher = ActiveSupport::EventedFileUpdateChecker
 end
